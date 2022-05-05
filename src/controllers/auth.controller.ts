1b5360bf8abe98933c7220b0e7cbb61b23e0ffeb
steve <sfreeman422@protonmail.com>
2022-05-05 11:27:58 -0400

    Moved auth functions into  auth service and consolidated to using axios everywhere possible

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,27 +1,13 @@
-/* eslint-disable @typescript-eslint/camelcase */
-import request from 'request';
 import express, { Router } from 'express';
-import * as querystring from 'querystring';
-import { v4 as uuidv4 } from 'uuid';
-import { UserService } from '../services/user/user.service';
+import { AuthService } from '../services/auth/auth.service';
 
 export const authController: Router = express.Router();
 
-const userService = new UserService();
+const authService = new AuthService();
+
 // Used to initiate authorization workflow for spotify.
 authController.get('/login', (_req, res) => {
-  const state = uuidv4();
-  const scope = 'user-read-private user-read-email playlist-modify-public user-top-read';
-  res.redirect(
-    'https://accounts.spotify.com/authorize?' +
-      querystring.stringify({
-        response_type: 'code',
-        client_id: process.env.SPOTIFY_CLIENT_ID,
-        scope: scope,
-        redirect_uri: process.env.SPOTIFY_TOKEN_REDIRECT_URL,
-        state: state,
-      }),
-  );
+  authService.loginWithScope(res);
 });
 
 // Used once an authorization token is given to use by spotify.
@@ -30,53 +16,10 @@ authController.get('/login/callback', (req, res) => {
   if (state === null) {
     res.status(500).send('State mismatch');
   } else {
-    const reqOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code,
-        redirect_uri: process.env.SPOTIFY_TOKEN_REDIRECT_URL,
-        grant_type: 'authorization_code',
-      },
-      headers: {
-        Authorization:
-          'Basic ' +
-          Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'),
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      json: true,
-    };
-
-    console.log(reqOptions);
-
-    request.post(reqOptions, (err, response, body) => {
-      console.log(response.statusCode);
-      console.log(response.statusMessage);
-      console.log(response.body);
-      if (!err && response.statusCode === 200) {
-        console.log('success');
-        console.log(body);
-        const access_token = body.access_token;
-        const refresh_token = body.refresh_token;
-
-        console.log(access_token);
-        console.log(refresh_token);
-
-        const options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { Authorization: 'Bearer ' + access_token },
-          json: true,
-        };
-
-        // use the access token to access the Spotify Web API
-        request.get(options, async (_e, _r, b) => {
-          console.log('end');
-          console.log(b);
-          await userService.saveUser(access_token, refresh_token, b.id).then(() => res.status(200).send());
-        });
-      } else {
-        console.log(err);
-        res.status(500).send(err);
-      }
-    });
+    // This should eventually route to a page where a user can see what playlist they spotify / own.
+    authService
+      .getUserDataAndSaveUser(code as string)
+      .then(() => res.status(200).send())
+      .catch(e => res.status(500).send(e));
   }
 });
--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,1 +1,121 @@
-export class AuthService {}
+/* eslint-disable @typescript-eslint/camelcase */
+import axios from 'axios';
+import { Response } from 'express';
+import * as querystring from 'querystring';
+import request from 'request';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from '../../shared/db/models/User';
+import { UserService } from '../user/user.service';
+
+export interface TokenSet {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface SpotifyUserDataExplicitContent {
+  filter_enabled: boolean;
+  filter_locked: boolean;
+}
+
+export interface SpotifyUserDataExternalUrls {
+  spotify: string;
+}
+
+export interface SpotifyUserDataFollowers {
+  href: string;
+  total: number;
+}
+
+export interface SpotifyUserData {
+  country: string;
+  display_name: string;
+  email: string;
+  explicit_content: SpotifyUserDataExplicitContent;
+  external_urls: SpotifyUserDataExternalUrls;
+  followers: SpotifyUserDataFollowers;
+  href: string;
+  id: string;
+  images: string[];
+  product: string;
+  type: string;
+  uri: string;
+}
+export class AuthService {
+  userService = new UserService();
+
+  loginWithScope(res: Response): void {
+    const state = uuidv4();
+    const scope = 'user-read-private user-read-email playlist-modify-public user-top-read';
+    res.redirect(
+      'https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+          response_type: 'code',
+          client_id: process.env.SPOTIFY_CLIENT_ID,
+          scope: scope,
+          redirect_uri: process.env.SPOTIFY_TOKEN_REDIRECT_URL,
+          state: state,
+        }),
+    );
+  }
+
+  getTokens(code: string): Promise<TokenSet> {
+    const reqOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code,
+        redirect_uri: process.env.SPOTIFY_TOKEN_REDIRECT_URL,
+        grant_type: 'authorization_code',
+      },
+      headers: {
+        Authorization:
+          'Basic ' +
+          Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'),
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      json: true,
+    };
+
+    /// Wish we could use Axios here to remove the request dependency but I could not get Axios to play nice with application/x-www-form-urlencoded when hitting spotify api.
+    return new Promise((resolve, reject) => {
+      request.post(reqOptions, (err, response, body) => {
+        if (!err && response.statusCode === 200) {
+          const accessToken = body.access_token;
+          const refreshToken = body.refresh_token;
+          resolve({ accessToken, refreshToken });
+        } else {
+          reject(err);
+        }
+      });
+    });
+  }
+
+  getUserData(accessToken: string): Promise<SpotifyUserData> {
+    return axios
+      .get('https://api.spotify.com/v1/me', {
+        headers: {
+          Authorization: 'Bearer ' + accessToken,
+        },
+      })
+      .then(response => {
+        return response.data;
+      })
+      .catch(e => {
+        console.error(e);
+        throw new Error(e);
+      });
+  }
+
+  async getUserDataAndSaveUser(code: string): Promise<User> {
+    const tokens = await this.getTokens(code).catch(e => {
+      console.error(e);
+      throw new Error(e);
+    });
+    const userData = await this.getUserData(tokens.accessToken).catch(e => {
+      console.error(e);
+      throw new Error(e);
+    });
+    console.log(tokens);
+    console.log(userData);
+    return this.userService.saveUser(tokens.accessToken, tokens.refreshToken, userData.id);
+  }
+}
