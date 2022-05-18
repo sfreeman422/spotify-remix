2b1885e98363a2e149e6f1c2188c641dc5180b4f
steve <sfreeman422@protonmail.com>
2022-05-17 22:35:00 -0400

    Removed the removePlaylist function and DELETE playlist route since this is a concern of spotify rather than remix

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 # TODO:
 
-- Get all songs in liked playlist recursively
 - Add ability to unsubscribe from a playlist
 - Consider unsubscribing users who no longer follow a given playlist
 - Consider a way to easily clean up orphaned playlists
--- src/services/spotify/spotify.service.ts
+++ src/services/spotify/spotify.service.ts
@@ -110,14 +110,6 @@ export class SpotifyService {
     });
   }
 
-  async removePlaylist(accessToken: string, playlists: string[]): Promise<Playlist[]> {
-    const ownedPlaylists = await this.userService.getAllOwnedPlaylists(accessToken);
-    const playlistsOwnedAndToBeDeleted: Playlist[] = ownedPlaylists.filter(x =>
-      playlists.some(y => x.playlistId === y),
-    );
-    return this.userService.deletePlaylist(playlistsOwnedAndToBeDeleted);
-  }
-
   unsubscribeFromPlaylist(_accessToken: string, _playlistId: string): void {
     console.log('not yet implemented');
   }
