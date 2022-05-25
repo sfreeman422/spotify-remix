2e927b94970a76e743b40e9364535759bfbbd32d
steve <sfreeman422@protonmail.com>
2022-05-25 08:14:52 -0400

    Fixed refresh-script.sh and token-script.sh

--- jobs/refresh/refresh-script.sh
+++ jobs/refresh/refresh-script.sh
@@ -1,4 +1,4 @@
 . /home/sfreeman/.bash_profile
 PATH=/usr/local/bin:$PATH
-cd /home/sfreeman/spotify-remix/job
-pipenv run python3.8 ./job.py
+cd /home/sfreeman/spotify-remix/jobs/refresh
+pipenv run python3.8 ./refresh-job.py
