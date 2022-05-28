34fc9f02294f71da738edcbf955e2cb8c6fc5624
steve <sfreeman422@protonmail.com>
2022-05-28 13:50:02 -0400

    Began writing tests for spotify.service

--- src/services/spotify/spotify.service.spec.ts
+++ src/services/spotify/spotify.service.spec.ts
@@ -1,65 +1,98 @@
+import { SpotifyHttpService } from './spotify-http.service';
+import { SpotifyUserData } from './spotify.generated.interface';
 import { SongWithUserData } from './spotify.interface';
 import { SpotifyService } from './spotify.service';
 
 describe('SpotifyService', () => {
-  let spotifyService = new SpotifyService();
+  let spotifyService: SpotifyService;
+  const mockSpotifyHttpService = {
+    getUserData: jest.fn(),
+    getUserPlaylists: jest.fn(),
+    createUserPLaylist: jest.fn(),
+    subscribeToPlaylist: jest.fn(),
+    getPlaylistTracks: jest.fn(),
+    removeAllPlaylistTracks: jest.fn(),
+    addSongsToPlaylist: jest.fn(),
+    getTopSongsByUser: jest.fn(),
+    getLikedSongsByUser: jest.fn(),
+  };
 
   beforeEach(() => {
     spotifyService = new SpotifyService();
+    spotifyService.httpService = (mockSpotifyHttpService as unknown) as SpotifyHttpService;
+  });
+
+  describe('getUserData()', () => {
+    it('should return user data when user data is returned', async () => {
+      expect.assertions(1);
+      const mockUser = { email: 'abc@123.com' } as SpotifyUserData;
+      jest.spyOn(spotifyService.httpService, 'getUserData').mockResolvedValueOnce(mockUser);
+      const userData = await spotifyService.getUserData('123');
+      expect(userData).toBe(mockUser);
+    });
+
+    it('should throw an error when an error is thrown', async () => {
+      expect.assertions(1);
+      const mockError = new Error('Test');
+      jest.spyOn(spotifyService.httpService, 'getUserData').mockRejectedValueOnce(mockError);
+      await spotifyService.getUserData('123').catch(e => {
+        expect(e).toBe(mockError);
+      });
+    });
   });
 
   describe('roundRobinSort()', () => {
     it('should round robin sort', () => {
       const unsorted = [
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
         {
-          accessToken: '3',
+          spotifyId: '3',
           uri: '3',
         },
         {
-          accessToken: '3',
+          spotifyId: '3',
           uri: '3',
         },
         {
-          accessToken: '2',
+          spotifyId: '2',
           uri: '2',
         },
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
       ] as SongWithUserData[];
 
       const sorted = [
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
         {
-          accessToken: '3',
+          spotifyId: '3',
           uri: '3',
         },
         {
-          accessToken: '2',
+          spotifyId: '2',
           uri: '2',
         },
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
         {
-          accessToken: '3',
+          spotifyId: '3',
           uri: '3',
         },
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
       ];
@@ -69,30 +102,30 @@ describe('SpotifyService', () => {
     it('should round robin sort when only one access token exists', () => {
       const unsorted = [
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
       ] as SongWithUserData[];
 
       const sorted = [
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
         {
-          accessToken: '1',
+          spotifyId: '1',
           uri: '1',
         },
       ] as SongWithUserData[];
