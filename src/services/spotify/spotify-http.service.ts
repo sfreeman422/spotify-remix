b5631260df32acad245486844f7d3edca9550ac1
sfreeman422 <sfreeman422@protonmail.com>
2022-12-05 09:08:25 -0500

    Removed ? on topSongs

--- src/services/spotify/spotify-http.service.ts
+++ src/services/spotify/spotify-http.service.ts
@@ -182,7 +182,7 @@ export class SpotifyHttpService {
           if (x?.data?.next) {
             return this.getTopSongsByUser(user, x?.data?.next).then((data: SongsByUser) => ({
               user: user,
-              topSongs: songs.concat(data?.topSongs),
+              topSongs: songs.concat(data.topSongs),
               likedSongs: [],
             }));
           }
