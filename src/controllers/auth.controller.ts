55552b6c013c2dde6b67d811b5248b8485ccdcc9
sfreeman422 <sfreeman422@protonmail.com>
2022-12-26 10:35:22 -0500

    Updated authController logs in standardized format

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,24 +7,69 @@ const authService = new AuthService();
 
 // Used to initiate authorization workflow for spotify.
 authController.get('/login', (_req, res) => {
-  console.log('authController /login route hit');
+  console.log(
+    JSON.stringify({
+      level: 'log',
+      name: 'authController',
+      route: '/login',
+      time: new Date(),
+      request: _req,
+      message: 'login route hit',
+    }),
+  );
   authService.loginWithScope(res);
 });
 
 // Used once an authorization token is given to use by spotify.
 authController.get('/login/callback', (req, res) => {
+  console.log(
+    JSON.stringify({
+      level: 'error',
+      name: 'authController',
+      route: '/login/callback',
+      time: new Date(),
+      request: req,
+      message: `login/callback route hit`,
+    }),
+  );
   const { code, state } = req.query;
   if (state === null) {
+    console.log(
+      JSON.stringify({
+        level: 'error',
+        name: 'authController',
+        route: '/login/callback',
+        time: new Date(),
+        request: req,
+        message: `missing code=${code} and state=${state} returning 500 for state mismatch`,
+      }),
+    );
     res.status(500).send('State mismatch');
   } else {
-    console.log('authController /login/callback route hit');
+    console.log(
+      JSON.stringify({
+        level: 'log',
+        name: 'authController',
+        route: '/login/callback',
+        time: new Date(),
+        request: req,
+        message: `calling authService.getUserDataAndSaveUser(${code})`,
+      }),
+    );
     // This should eventually route to a page where a user can see what playlist they spotify / own.
     authService
       .getUserDataAndSaveUser(code as string)
       // We should maybe not put accessToken and refreshToken here but...
       .then(x => {
         console.log(
-          `redirecting to /dashboard?accessToken=${x.accessToken}&refreshToken=${x.refreshToken}&spotifyId=${x.spotifyId}`,
+          JSON.stringify({
+            level: 'log',
+            name: 'authController',
+            route: '/login/callback',
+            time: new Date(),
+            request: req,
+            message: `authService.getUserDataAndSaveUser was successful - redirecting to /dashboard?accessToken=${x.accessToken}&refreshToken=${x.refreshToken}&spotifyId=${x.spotifyId}`,
+          }),
         );
         res.redirect(`/dashboard?accessToken=${x.accessToken}&refreshToken=${x.refreshToken}&spotifyId=${x.spotifyId}`);
       })
@@ -37,6 +82,16 @@ authController.get('/login/callback', (req, res) => {
 });
 
 authController.get('/refresh', async (req, res) => {
+  console.log(
+    JSON.stringify({
+      level: 'log',
+      name: 'authController',
+      route: '/refresh',
+      time: new Date(),
+      request: req,
+      message: `/refresh route hit`,
+    }),
+  );
   const { authorization, refreshtoken } = req.headers;
   const { spotifyId } = req.query;
   const accessToken = authorization ? authorization.split(' ')[1] : undefined;
@@ -51,10 +106,16 @@ authController.get('/refresh', async (req, res) => {
         res.status(500).send(e);
       });
   } else {
-    console.log('returning 400');
-    console.log('isAuthorization', !!authorization);
-    console.log('isSpotifyId', !!spotifyId);
-    console.log('isRefreshToken', !!refreshtoken);
+    console.log(
+      JSON.stringify({
+        level: 'log',
+        name: 'authController',
+        route: '/refresh',
+        time: new Date(),
+        request: req,
+        message: `returning 400 - missing authorization=${!!authorization}, missing spotifyId=${!!spotifyId} or missing refreshToken=${!!refreshtoken}`,
+      }),
+    );
     res.status(400).send('Missing authorization header or spotifyId');
   }
 });
